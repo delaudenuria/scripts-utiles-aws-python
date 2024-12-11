670e70339044c94e25ca6ea844c7a1a23658dd69
delaudenuria <delaudenuria@gmail.com>
2024-12-11 17:34:52 -0300

    Update get-users-from-group-cognito.py

--- get-users-from-group-cognito.py
+++ get-users-from-group-cognito.py
@@ -1,19 +1,14 @@
 import boto3
 import json
-from datetime import datetime
 
-# Configuración directamente en el código
-OUTPUT_FILE = ''  # El archivo CSV donde se guardarán los usuarios
 ACCESS_KEY = ''  # Acceso programático (Access Key)
 SECRET_KEY = ''  # Secret Key
-SESSION_TOKEN = ''  # Token de sesión si usas credenciales temporales (opcional)
+SESSION_TOKEN = ''  # Token de sesión 
+user_pool_id = ''  # Reemplaza con tu User Pool ID
+group_name = ''  # Reemplaza con el nombre del grupo
 
 # Crear un cliente de Cognito
 client = boto3.client('cognito-idp')
-
-# Parámetros
-user_pool_id = ''  # Reemplaza con tu User Pool ID
-group_name = ''  # Reemplaza con el nombre del grupo
 
 def list_users_in_group(user_pool_id, group_name):
     try:
@@ -23,16 +18,10 @@ def list_users_in_group(user_pool_id, group_name):
             GroupName=group_name
         )
 
-        # Función para convertir datetime a string
-        def convert_datetime(obj):
-            if isinstance(obj, datetime):
-                return obj.isoformat()  # Convertir datetime a formato string
-            raise TypeError("Type not serializable")
-
-        # Guardar la respuesta en un archivo JSON, usando el conversor para datetime
+        # Guardar la respuesta en un archivo JSON
         with open(f"{group_name}.json", 'w') as file:
-            json.dump(response, file, default=convert_datetime, indent=4)
-        # Genera un archivo con el mismo nombre del grupo
+            json.dump(response, file, default=str, indent=4)
+
         print(f"Usuarios del grupo {group_name} guardados en {group_name}.json")
 
     except Exception as e:
